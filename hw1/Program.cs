cebffcda40ec2dfe11aca1d68b2f542c84aa04e0
Alexander Timirov <yudyymal@gmail.com>
2022-09-08 20:54:49 +0500

    Homework 1

---
+++ hw1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+
+if (num>999 || num<100)
+{
+    Console.WriteLine("Число не является трёхзначным или отрицательное");
+}
+else
+{
+    Console.WriteLine
+    ("Вторая цифра в числе - " + (num.ToString().ToCharArray()[1]));
+}
